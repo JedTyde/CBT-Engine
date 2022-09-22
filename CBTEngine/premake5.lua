0ae63e6afa6b3d649fee30d0d0bf19d5fd427c67
JedTyde <59925703+JedTyde@users.noreply.github.com>
2022-09-23 00:18:47 +0200

    2 menos pa 10 yeeeeeeee
    must protect rebecca must protect rebecca must protect rebecca must protect rebecca

---
+++ CBTEngine/premake5.lua
@@ -0,0 +1,107 @@
+workspace "CBTEngine"
+architecture "x64"
+
+configurations
+{
+    "Debug",
+    "Release",
+    "Dist"
+}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CBTEngine"
+    location "CBTEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+    
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CBT_PLATFORM_WINDOWS",
+            "CBT_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "CBT_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CBT_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CBT_DIST"
+        optimize "On"
+
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+    
+    includedirs
+    {
+        "CBTEngine/vendor/spdlog/include",
+        "CBTEngine/src"
+    }
+
+    links
+    {
+        "CBTEngine"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "CBT_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "CBT_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "CBT_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "CBT_DIST"
+        optimize "On"
