86ee383e3859baf8d0d9cb51a35832509918a57b
Pau <73171744+PaGaQi@users.noreply.github.com>
2022-10-18 09:04:25 +0200

    EEEEL NOME EL NOME DEL ENGINE SISISISISISIS
    AORA LA BENTANA TA VIEN

--- CBTEngine/CBTEngine/src/CBT/Window.h
+++ CBTEngine/CBTEngine/src/CBT/Window.h
@@ -12,7 +12,7 @@ namespace CBT {
 		unsigned int Width;
 		unsigned int Height;
 
-		WindowProps(const std::string& title = "Hazel Engine",
+		WindowProps(const std::string& title = "CBT Engine",
 			unsigned int width = 1280,
 			unsigned int height = 720)
 			: Title(title), Width(width), Height(height)
