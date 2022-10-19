18d698676edc5281f3e7990eadd841b0e50eb425
JedTyde <59925703+JedTyde@users.noreply.github.com>
2022-10-19 10:58:02 +0200

    No se uqe mierda estoy haciendo portfavor aiuda

--- CBTEngine/Sandbox/src/Layers/EditorLayer.cpp
+++ CBTEngine/Sandbox/src/Layers/EditorLayer.cpp
@@ -1,1 +1,63 @@
 #include "EditorLayer.h"
+
+#include "../vendor/imgui/imgui.h"
+#include <stdio.h>
+
+#include "CBT/Application.h"
+#include "CBT/Events/Event.h"
+
+EditorLayer::EditorLayer() : Layer("Editor Layer")
+{
+
+}
+
+EditorLayer::~EditorLayer()
+{
+
+}
+
+void EditorLayer::OnAttach()
+{
+	m_About = std::make_shared<AboutPanel>();
+}
+
+void EditorLayer::OnDetach()
+{
+	m_Panels.clear();
+}
+
+void EditorLayer::OnUpdate()
+{
+	if (m_Configuration->active)
+		m_Configuration->Update();
+	if (m_Assets->active)
+		m_Assets->Update();
+}
+
+void EditorLayer::OnImGuiRender()
+{
+
+
+
+}
+
+void EditorLayer::OnEvent(CBT::Event& ev)
+{
+
+}
+
+void EditorLayer::MainMenuBar()
+{
+
+
+}
+
+void EditorLayer::DockSpace()
+{
+
+}
+
+bool EditorLayer::OnKeyPressed(CBT::KeyPressedEvent& k)
+{
+
+}
--- CBTEngine/Sandbox/src/Layers/EditorLayer.h
+++ CBTEngine/Sandbox/src/Layers/EditorLayer.h
@@ -3,9 +3,31 @@
 #include <CBT/Layer.h>
 
 #include "Panels/Panels.h"
+#include "Panels/AboutPanel.h"
+#include "CBT/Events/KeyEvent.h"
+#include "CBT/Events/Event.h"
 
 
-class EditorLayer
+class EditorLayer : public CBT::Layer
 {
+	EditorLayer();
+	~EditorLayer();
+
+	void OnAttach() override;
+	void OnDetach() override;
+	void OnUpdate() override;
+	void OnImGuiRender() override;
+	void OnEvent(CBT::Event& ev) override;
+
+private:
+	void MainMenuBar();
+	void DockSpace();
+
+	bool OnKeyPressed(CBT::KeyPressedEvent& k);
+
+	bool m_ShowConsole = false;
+	bool m_ShowDemo = false;
+
+	std::shared_ptr<AboutPanel>			m_About = nullptr;
 };
 
